579861e8cac453e20c192229db68b1d871ee6ce2
prakao <kpkrisrin@gmail.com>
2026-01-01 22:13:57 +0700

    lab12_2_finished

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int a,int b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
 }
