b2ebcae8975de337026a34ae9883bb710f3f86ab
prakao <kpkrisrin@gmail.com>
2026-01-01 21:57:10 +0700

    Lab12_1_finished

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int num){
+    if(num == 0)return 0;
+    else if(num == 1) return 1;
+    else return fibonacci(num-1)+fibonacci(num-2);
 }
